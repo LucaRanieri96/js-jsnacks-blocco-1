a9d304dfbaca57c10cfdcb93cf84faf3a62638f7
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-14 11:51:05 +0100

    result with better innerHtml

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -8,8 +8,8 @@ const result = document.getElementById("result");
 
 if (number1 > number2) {
     console.log (number1)
-    result.innerHTML = ("risultato" + number1)
+    result.innerHTML = ("risultato = " + number1)
  } else {
     console.log (number2)
-    result.innerHTML = ("risultato" + number2)
+    result.innerHTML = ("risultato = " + number2)
 }
