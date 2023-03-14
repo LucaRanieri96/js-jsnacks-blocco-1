9188854394944189181066b6fca001ec403b2a69
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-14 11:46:26 +0100

    jsnack1

---
+++ jsnack1/script.js
@@ -0,0 +1,12 @@
+const number1 = Number(prompt ("inserisci un numero"));
+console.log(number1)
+
+const number2 = Number(prompt ("inserisci un secondo numero"));
+console.log(number2)
+
+
+if (number1 > number2) {
+    console.log (number1)
+ } else {
+    console.log (number2)
+}
