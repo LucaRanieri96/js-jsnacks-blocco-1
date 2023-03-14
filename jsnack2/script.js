e8f3a95bb0bcf026a572022de47ad60f4239fe0c
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-14 12:10:48 +0100

    fix jsnack2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -4,9 +4,9 @@ console.log(firstWord)
 const secondWord = prompt ("inserisci un'altra parola");
 console.log(secondWord)
 
-if (firstWord > secondWord) {
+if (firstWord.length > secondWord.length) {
     console.log (firstWord + secondWord)
-} else if(secondWord > firstWord) {
+} else if(secondWord.length > firstWord.length) {
     console.log (secondWord + firstWord)
 } else {
     console.log (firstWord + secondWord)
