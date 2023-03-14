78b014b42db2649d167701c207e8d86a77cb003e
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-14 11:50:28 +0100

    jsnack1 with innerHTML

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -4,9 +4,12 @@ console.log(number1)
 const number2 = Number(prompt ("inserisci un secondo numero"));
 console.log(number2)
 
+const result = document.getElementById("result");
 
 if (number1 > number2) {
     console.log (number1)
+    result.innerHTML = ("risultato" + number1)
  } else {
     console.log (number2)
+    result.innerHTML = ("risultato" + number2)
 }
