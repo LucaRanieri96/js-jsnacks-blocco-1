046c3cce61fc887673702d3a285cbbcd5ef96120
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-15 12:28:37 +0100

    jsnack4

---
+++ jsnack4/main.js
@@ -0,0 +1,45 @@
+// Snack 4 (sia con for che con while):
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+// array con gli invitati
+const invitedPeople = [
+    "paolo",
+    "francesca",
+    "giovanni",
+    "ferdinando",
+    "claudia",
+    "Alessandro"
+]
+
+// chiedo all'utente il suo nome
+const userName = prompt("Qual è il tuo nome?");
+
+// ciclo for
+
+let invited = false;
+
+for (let i = 0; i < invitedPeople.length; i++) {
+
+    if (invitedPeople[i] === userName) {
+    invited = true;
+}
+
+// variante while
+
+let invited = false;
+let j = 0;
+
+while (j < invitedPeople.length) {
+
+    if (invitedPeople[j] === userName) {
+    invited = true;
+    }
+    j++
+}
+
+// dico all'utente se è invitato o meno
+if (invited) {
+alert("Benvenuto alla festa del grande Gatsby!");
+} else {
+alert("Mi dispiace, non sei stato invitato alla festa del grande Gatsby.");
+};
