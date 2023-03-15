4628e425c750e0a4942adef2c9e019f32a128e97
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-03-15 11:50:49 +0100

    update snack3 with while loop

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -12,3 +12,25 @@ for (let i = 0; i < 10; i++) {
 console.log("la somma dei numeri: " + numberSum)
 
 Sum.innerHTML = "la somma dei numeri = " + numberSum;
+
+// WHILE:LOOP
+
+// Il software deve chiedere per 10 volte all’utente di inserire un numero.
+const Somma = document.getElementById("somma");
+
+let sommaDeiNumeri = 0 
+
+let j = 0;
+
+while ( j < 10) {
+
+    const numero = Number(prompt("inserisci un numero(ti verrà chiesto 10 volte)"));
+    sommaDeiNumeri = sommaDeiNumeri + numero;
+
+    j++
+}
+
+// Il programma stampa la somma di tutti i numeri inseriti.
+console.log("la somma dei numeri: " + sommaDeiNumeri)
+
+Sum.innerHTML = "la somma dei numeri = " + sommaDeiNumeri;
